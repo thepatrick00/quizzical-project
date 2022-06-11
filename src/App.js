896a3fb20446b5381e4732aa6d16a9acd70f5cdd
Patrick <patrick@meetpatrick.dev>
2022-06-11 17:35:38 -0500

    for netlify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import Quiz from './components/Quiz'
 import Home from './components/Home'
-import About from './components/About'
 import './styles.css'
 
 export default function App() {
